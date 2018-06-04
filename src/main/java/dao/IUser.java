9356f48c545b66978df8ab02b3422dbad36423fe
fankaljead <728678732@qq.com>
2018-06-04 21:24:15 +0800

    Mybatis-curd的小demo

--- src/main/java/dao/IUser.java
+++ src/main/java/dao/IUser.java
@@ -1,12 +1,16 @@
 package dao;
 
 import models.User;
-import org.apache.ibatis.annotations.Select;
 
-/**
- * 使用注解方式之
- */
+import java.util.List;
+
+
 public interface IUser {
-    @Select("select * from user where id= #{id}")
-    public User getUserByID(int id);
+//    @Select("select * from user where id= #{id}")
+//    public User getUserByID(int id);
+    public List<User> getUserList();
+    public void insertUser(User user);
+    public void updateUser(User user);
+    public void deleteUser(int id);
+    public User getUser(int id);
 }
